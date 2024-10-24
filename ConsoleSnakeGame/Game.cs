347b9fc640180f236338c2bde20e68485a754905
Vitalii Voitovych <vitalii.voitovych@protonmail.com>
2024-10-24 17:09:06 +0300

    Added restart game

--- ConsoleSnakeGame/Game.cs
+++ ConsoleSnakeGame/Game.cs
@@ -1,8 +1,13 @@
-﻿namespace ConsoleSnakeGame;
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace ConsoleSnakeGame;
 
 public class Game
 {
     private GameField _gameField;
+
+    private bool _isRestarted = false;
+
     public static ConsoleKey PressedKey;
     public static bool IsGameOver = false;
 
@@ -11,19 +16,43 @@ public class Game
         _gameField = new();
     }
 
+    private void Reset()
+    {
+        Console.Clear();
+        _gameField = new GameField();
+        IsGameOver = false;
+        _isRestarted = false;
+    }
+
+    private void ShowRestartMessage()
+    {
+        Console.SetCursorPosition(GameField.COLUMNS + 4, 2);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("Restart the game press R:");
+    }
+
     public void Start()
     {
-        while (!IsGameOver)
+        ConsoleKey key;
+        do
         {
-            if (Console.KeyAvailable)
+            if (_isRestarted) Reset();
+            while (!IsGameOver)
             {
-                PressedKey = Console.ReadKey(true).Key;
+                if (Console.KeyAvailable)
+                {
+                    PressedKey = Console.ReadKey(true).Key;
+                }
+
+                _gameField.Draw();
+                _gameField.Update();
+                Thread.Sleep(150);
             }
 
-            _gameField.Draw();
-            _gameField.Update();
-            Thread.Sleep(150);
+            ShowRestartMessage();
+            key = Console.ReadKey(true).Key;
+            _isRestarted = key == ConsoleKey.R;
         }
-        Console.ReadKey();
+        while (_isRestarted);
     }
 }
